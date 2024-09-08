a8773df4447aab8cf0b70a35b56cfa4a8e3ac2bb
scandolara-lucas <lucasscandolara16@gmail.com>
2024-09-08 15:06:54 +0200

    add flutter plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -360,4 +360,19 @@ return {
       }
     end,
   },
+
+  { -- Undo tree stuff
+    "mbbill/undotree",
+    event = "VeryLazy",
+  },
+
+  { -- Fluter dev
+    "akinsho/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = true,
+  },
 }
