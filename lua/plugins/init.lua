9fd1d34b26583764117b836887d6f1f2fd7796b7
Lucas Scandolara <lucas.scandolara@iconpro.com>
2024-09-25 10:22:50 +0200

    follow url to browser on MacOS

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -110,6 +110,18 @@ return {
         substitutions = {},
       },
       new_notes_location = "current_dir",
+
+      -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
+      -- URL it will be ignored but you can customize this behavior here.
+      ---@param url string
+      follow_url_func = function(url)
+        -- Open the URL in the default web browser.
+        vim.fn.jobstart { "open", url } -- Mac OS
+        -- vim.fn.jobstart({"xdg-open", url})  -- linux
+        -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
+        -- vim.ui.open(url) -- need Neovim 0.10.0+
+      end,
+
       -- Optional, customize how note IDs are generated given an optional title.
       ---@param title string|?
       ---@return string
